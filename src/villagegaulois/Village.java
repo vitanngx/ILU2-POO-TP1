2f5db03a670777ea82c7aeac96daa6c411e36322
Tan <tan@macbookair.home>
2026-09-24 14:20:00 +0200

    TP1 : Implémentation de la classe interne Marche et mise à jour de Village

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -8,10 +8,13 @@ public class Village {
 	private Chef chef;
 	private Gaulois[] villageois;
 	private int nbVillageois = 0;
+	private Marche marche; // marche appartient à la 
 
-	public Village(String nom, int nbVillageoisMaximum) {
+	// them so luong sap vao trong 
+	public Village(String nom, int nbVillageoisMaximum, int nbEtals) {
 		this.nom = nom;
 		villageois = new Gaulois[nbVillageoisMaximum];
+		this.marche = new Marche(nbEtals); // tao khu cho voi so luong sap tuong 
 	}
 
 	public String getNom() {
@@ -30,7 +33,7 @@ public class Village {
 	}
 
 	public Gaulois trouverHabitant(String nomGaulois) {
-		if (nomGaulois.equals(chef.getNom())) {
+		if (chef != null && nomGaulois.equals(chef.getNom())) {
 			return chef;
 		}
 		for (int i = 0; i < nbVillageois; i++) {
@@ -46,14 +49,141 @@ public class Village {
 		StringBuilder chaine = new StringBuilder();
 		if (nbVillageois < 1) {
 			chaine.append("Il n'y a encore aucun habitant au village du chef "
-					+ chef.getNom() + ".\n");
+					+ (chef != null ? chef.getNom() : "") + ".\n");
 		} else {
-			chaine.append("Au village du chef " + chef.getNom()
+			chaine.append("Au village du chef " + (chef != null ? chef.getNom() : "")
 					+ " vivent les légendaires gaulois :\n");
 			for (int i = 0; i < nbVillageois; i++) {
 				chaine.append("- " + villageois[i].getNom() + "\n");
 			}
 		}
 		return chaine.toString();
+	}
+
+	// 2, On rangeles places pour les vendeurs
+	public String installerVendeur(Gaulois vendeur, String produit, int nbProduit) {
+		StringBuilder chaine = new StringBuilder();
+		chaine.append(vendeur.getNom() + " cherche un endroit pour vendre " + nbProduit + " " + produit + ".\n");
+		
+		int indiceEtal = marche.trouverEtalLibre(); //hoi cho xem con cho trong ko
+		if (indiceEtal != -1) {
+			marche.utiliserEtal(indiceEtal, vendeur, produit, nbProduit);
+			chaine.append("Le vendeur " + vendeur.getNom() + " vend des " + produit + " à l'étal n° " + (indiceEtal + 1) + ".\n");
+		} else {
+			chaine.append("Il n'y a plus d'étal libre au marché.\n");
+		}
+		return chaine.toString();
+	}
+
+	// 3. On cherche la liste des vendeurs qui vendent le produit
+	public String rechercherVendeursProduit(String produit) {
+		StringBuilder chaine = new StringBuilder();
+		Etal[] etals = marche.trouverEtals(produit);
+		if (etals.length == 0) {
+			chaine.append("Il n'y a pas de vendeur qui propose des " + produit + " au marché.\n");
+		} else if (etals.length == 1) {
+			chaine.append("Seul le vendeur " + etals[0].getVendeur().getNom() + " propose des " + produit + " au marché.\n");
+		} else {
+			chaine.append("Les vendeurs qui proposent des " + produit + " sont :\n");
+			for (int i = 0; i < etals.length; i++) {
+				chaine.append("- " + etals[i].getVendeur().getNom() + "\n");
+			}
+		}
+		return chaine.toString();
+	}
+
+	// 4. On cherche etal de quelques vendeurs
+	public Etal rechercherEtal(Gaulois gaulois) {
+		return marche.trouverVendeur(gaulois);
+	}
+
+	// 5. Laisse partir vendeur
+	public String partirVendeur(Gaulois gaulois) {
+		Etal etal = marche.trouverVendeur(gaulois);
+		if (etal != null) {
+			return etal.libererEtal();
+		}
+		return null;
+	}
+
+	// 6. Etat du Marche
+	public String afficherMarche() {
+		StringBuilder chaine = new StringBuilder();
+		chaine.append("Le marché du village \"" + nom + "\" possède plusieurs étals :\n");
+		chaine.append(marche.afficherMarche());
+		return chaine.toString();
+	}
+
+	// LỚP LỒNG NHAU (CLASSE INTERNE) QUẢN LÝ KHU CHỢ
+	private static class Marche {
+		private Etal[] etals; // Mảng chứa các sạp hàng
+
+		private Marche(int nbEtals) {
+			etals = new Etal[nbEtals];
+			for (int i = 0; i < nbEtals; i++) {
+				etals[i] = new Etal();
+			}
+		}
+
+		// Giao sạp cho người bán
+		private void utiliserEtal(int indiceEtal, Gaulois vendeur, String produit, int nbProduit) {
+			etals[indiceEtal].occuperEtal(vendeur, produit, nbProduit);
+		}
+
+		// Tìm sạp còn trống (trả về vị trí 0, 1, 2... hoặc -1 nếu hết chỗ)
+		private int trouverEtalLibre() {
+			for (int i = 0; i < etals.length; i++) {
+				if (!etals[i].isEtalOccupe()) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		// Tìm tất cả các sạp đang bán cùng 1 món hàng
+		private Etal[] trouverEtals(String produit) {
+			int nb = 0;
+			for (int i = 0; i < etals.length; i++) {
+				if (etals[i].isEtalOccupe() && etals[i].contientProduit(produit)) {
+					nb++;
+				}
+			}
+			Etal[] etalsProduit = new Etal[nb];
+			int index = 0;
+			for (int i = 0; i < etals.length; i++) {
+				if (etals[i].isEtalOccupe() && etals[i].contientProduit(produit)) {
+					etalsProduit[index] = etals[i];
+					index++;
+				}
+			}
+			return etalsProduit;
+		}
+
+		// Tìm sạp của một người dân xem người đó ngồi đâu
+		private Etal trouverVendeur(Gaulois gaulois) {
+			for (int i = 0; i < etals.length; i++) {
+				if (etals[i].isEtalOccupe() && etals[i].getVendeur() == gaulois) {
+					return etals[i];
+				}
+			}
+			return null;
+		}
+
+		// In ra danh sách các sạp và số sạp còn trống
+		private String afficherMarche() {
+			StringBuilder chaine = new StringBuilder();
+			int nbEtalVide = 0;
+			for (int i = 0; i < etals.length; i++) {
+				if (etals[i].isEtalOccupe()) {
+					chaine.append(etals[i].afficherEtal());
+				} else {
+					nbEtalVide++;
+				}
+			}
+			if (nbEtalVide > 0) {
+				chaine.append("Il reste " + nbEtalVide + " étals non utilisés dans le marché.\n");
+			}
+			return chaine.toString();
+		}
 	}
 }
